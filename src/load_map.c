2bb08683c153b1a0ec7a23dd094ae8cdf47c33e2
VOLTE <thibault2.brumaire@epitech.eu>
2020-12-07 18:12:05 +0100

    🔨 edited 0 -> 1 to fix y value

--- src/load_map.c
+++ src/load_map.c
@@ -11,13 +11,13 @@
 void load_boats(char map[2][8][8], char buff[32])
 {
     for (int i = buff[10] - 'A'; i <= buff[13] - 'A'; i++)
-        for (int b = buff[11] - '0'; b <= buff[14] - '0'; b++)
+        for (int b = buff[11] - '1'; b <= buff[14] - '1'; b++)
             map[0][i][b] = '3';
     for (int i = buff[18] - 'A'; i <= buff[21] - 'A'; i++)
-        for (int b = buff[19] - '0'; b <= buff[22] - '0'; b++)
+        for (int b = buff[19] - '1'; b <= buff[22] - '1'; b++)
             map[0][i][b] = '4';
     for (int i = buff[26] - 'A'; i <= buff[29] - 'A'; i++)
-        for (int b = buff[27] - '0'; b <= buff[30] - '0'; b++)
+        for (int b = buff[27] - '1'; b <= buff[30] - '1'; b++)
             map[0][i][b] = '5';
 }
 
@@ -32,8 +32,8 @@ void load_map(char *path, char map[2][8][8])
             map[0][i][b] = '.';
             map[1][i][b] = '.';
         }
-    map[0][buff[2] - 'A'][buff[3]- '0'] = '2';
-    map[0][buff[5] - 'A'][buff[6]- '0'] = '2';
+    map[0][buff[2] - 'A'][buff[3] - '1'] = '2';
+    map[0][buff[5] - 'A'][buff[6] - '1'] = '2';
     load_boats(map, buff);
 }
 
