95a77ce19aa4e138c3e335c138183e318f2afe77
VOLTE <thibault2.brumaire@epitech.eu>
2020-12-07 15:12:12 +0100

    🔨 exit if error opening file

--- src/my_futils.c
+++ src/my_futils.c
@@ -16,6 +16,8 @@ int my_fopen(const char *filepath, const char accesmode)
         descriptor = open(filepath, O_WRONLY);
     else
         descriptor = open(filepath, O_RDWR);
+    if (descriptor == -1)
+        my_ferror("Could not open the file");
     return (descriptor);
 }
 
