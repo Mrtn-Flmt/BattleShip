4917085aee799167fdcefa91a14cb5d0f8c5bfbc
VOLTE <thibault2.brumaire@epitech.eu>
2020-12-17 00:50:10 +0100

    🔨 true pid connection

--- src/my_connect_utils.c
+++ src/my_connect_utils.c
@@ -8,13 +8,29 @@
 #include "../include/my.h"
 #include "../include/navy.h"
 
-void connect_process(int ac)
+static int p2 = 0;
+
+void sig_user2(int signal, siginfo_t *siginfo, void *data)
 {
+    p2 = siginfo->si_pid;
+}
+
+void connect_process(net *pids)
+{
+    struct sigaction signal;
+
     my_printf("my_pid: %d\n", getpid());
-    if (ac < 3) {
-        my_printf("waiting for ennemy connection...\n\n");
-        my_printf("enemy connected\n");
+    if (pids->pone) {
+        signal.sa_sigaction = &sig_user2;
+        signal.sa_flags = SA_SIGINFO;
+        sigaction(SIGUSR1, &signal, NULL);
+        my_printf("waiting for ennemy connection...");
+        pause();
+        pids->p2 = p2;
+        my_printf("\n\nenemy connected\n\n");
     } else {
-        my_printf("successfully connected\n");
+        if (kill(pids->p1, SIGUSR1) == -1)
+            my_ferror("Connection failed");
+        my_printf("successfully connected\n\n");
     }
 }
