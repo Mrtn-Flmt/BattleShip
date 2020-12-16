cc1bb9a69c26e8ddf444770109fb85566194da60
VOLTE <thibault2.brumaire@epitech.eu>
2020-12-17 00:47:13 +0100

    🔨 unused struct and added new funct

--- include/navy.h
+++ include/navy.h
@@ -9,23 +9,19 @@
 #define _NAVY
 #include <sys/stat.h>
 #include <ucontext.h>
-typedef struct stat st;
-typedef struct pos {
-    int x;
-    int y;
-} pos;
-typedef struct coords {
-    pos a;
-    pos b;
-} coords;
 
-/* USE THIS TO KILL A PROCESS WITH PID        
-    kill(my_getnbr(path), SIGUSR1);
-*/
-//case bateau = 'val bateau' case vide = '.' case touchée = 'x'
+typedef struct stat st;
+
+typedef struct net {
+    int p1;
+    int p2;
+    int pone;
+    int pnb;
+} net;
+
 void init_st(st *st, int ac, char *av[]);
 char *check_arg(int ac, char *av[]);
-int check_files(char buff[32]);
+int check_file(char buff[32]);
 
 int my_fopen(const char *filepath, const char accesmode);
 int my_fclose(int file);
@@ -34,8 +30,28 @@ int my_fscanf(int file, const char *format, ...);
 
 void load_map(char *path, char map[2][8][8]);
 void show_map(char map[2][8][8]);
-void print_win(int win);
-void game(char map[2][8][8]);
 
-void connect_process(int ac);
+void connect_process(net *pids);
+int get_mode(int signal);
+void send_mode(int signal, siginfo_t *siginfo, void *data);
+int check_move(char move[4]);
+char *ask_move(char move[4]);
+void send_move(char move[4], int p2id);
+void get_move(char move[4], int curent_sig);
+
+
+int check_pos(char c);
+int check_hit(char map[2][8][8], char move[4], int x[2]);
+int apply_move(char map[2][8][8], char move[4], int mode);
+
+void game(net pids, char map[2][8][8]);
+void game_p1(net pids, char map[2][8][8], int *win);
+void game_p2(net pids, char map[2][8][8], int *win);
+
+void print_win(int win);
+int check_win(char map[2][8][8]);
+
+void send_hit(int hit, int p2id);
+int get_hit(int curent_sig);
+void next_move(int signum);
 #endif
